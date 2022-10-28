1e536de427096bac3f1d3629f59765faf1a3a13e
Henrique <henryyquemenezes@hotmail.com>
2022-10-28 19:19:01 -0300

    Alteração no formulário de login.

---
+++ View/FormLogin.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace projetofinal
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btlogin = new System.Windows.Forms.Button();
+            this.cbUsuario = new System.Windows.Forms.ComboBox();
+            this.lbSair = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lbAlunosCadastrados = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbSenha
+            // 
+            this.tbSenha.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbSenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tbSenha.Location = new System.Drawing.Point(685, 355);
+            this.tbSenha.MaxLength = 30;
+            this.tbSenha.Name = "tbSenha";
+            this.tbSenha.Size = new System.Drawing.Size(328, 29);
+            this.tbSenha.TabIndex = 1;
+            this.tbSenha.UseSystemPasswordChar = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(685, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btlogin
+            // 
+            this.btlogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btlogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(138)))), ((int)(((byte)(196)))));
+            this.btlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btlogin.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btlogin.ForeColor = System.Drawing.Color.White;
+            this.btlogin.Location = new System.Drawing.Point(753, 440);
+            this.btlogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btlogin.Name = "btlogin";
+            this.btlogin.Size = new System.Drawing.Size(209, 59);
+            this.btlogin.TabIndex = 2;
+            this.btlogin.Text = "Login";
+            this.btlogin.UseVisualStyleBackColor = false;
+            this.btlogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // cbUsuario
+            // 
+            this.cbUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.cbUsuario.FormattingEnabled = true;
+            this.cbUsuario.Items.AddRange(new object[] {
+            "Selecione"});
+            this.cbUsuario.Location = new System.Drawing.Point(685, 298);
+            this.cbUsuario.Name = "cbUsuario";
+            this.cbUsuario.Size = new System.Drawing.Size(328, 29);
+            this.cbUsuario.TabIndex = 0;
+            // 
+            // lbSair
+            // 
+            this.lbSair.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbSair.Image = ((System.Drawing.Image)(resources.GetObject("lbSair.Image")));
+            this.lbSair.Location = new System.Drawing.Point(1112, 525);
+            this.lbSair.Name = "lbSair";
+            this.lbSair.Size = new System.Drawing.Size(40, 37);
+            this.lbSair.TabIndex = 3;
+            this.lbSair.Click += new System.EventHandler(this.lbSair_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox2.BackColor = System.Drawing.Color.Lavender;
+            this.pictureBox2.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(503, 574);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(639, 296);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 34);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 50;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(639, 352);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 34);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 51;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lbAlunosCadastrados
+            // 
+            this.lbAlunosCadastrados.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbAlunosCadastrados.BackColor = System.Drawing.Color.Lavender;
+            this.lbAlunosCadastrados.Font = new System.Drawing.Font("Segoe UI Semibold", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lbAlunosCadastrados.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(138)))), ((int)(((byte)(196)))));
+            this.lbAlunosCadastrados.Location = new System.Drawing.Point(0, 182);
+            this.lbAlunosCadastrados.Name = "lbAlunosCadastrados";
+            this.lbAlunosCadastrados.Size = new System.Drawing.Size(503, 73);
+            this.lbAlunosCadastrados.TabIndex = 52;
+            this.lbAlunosCadastrados.Text = "Seja bem vindo(a)!";
+            this.lbAlunosCadastrados.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(138)))), ((int)(((byte)(196)))));
+            this.label1.Location = new System.Drawing.Point(0, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(503, 63);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "Consulte outro professor para\r\nrealizar seu cadastro pessoal!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.BackColor = System.Drawing.Color.Lavender;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(503, 48);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Primeiro acesso?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(1164, 571);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbAlunosCadastrados);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.lbSair);
+            this.Controls.Add(this.tbSenha);
+            this.Controls.Add(this.cbUsuario);
+            this.Controls.Add(this.btlogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(1182, 618);
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormLogin_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbSenha;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btlogin;
+        private System.Windows.Forms.ComboBox cbUsuario;
+        private System.Windows.Forms.Label lbSair;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lbAlunosCadastrados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ View/FormLogin.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using academia.Class;
+using academia.DAO;
+using academia.Properties;
+
+namespace projetofinal
+{
+    public partial class FormLogin : Form
+    {
+        Conexao conec = new Conexao();
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            ProfessorDAO professorDAO = new ProfessorDAO();
+            cbUsuario.DataSource = professorDAO.listarProfs();
+            cbUsuario.DisplayMember = "Usuário";
+            cbUsuario.Text = "henrique-mo";
+            tbSenha.Text = "321654";
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {//btLogin
+            try
+            {
+                SqlConnection conexao = new SqlConnection(conec.ConexaoBD());
+                string sql = @"SELECT * FROM professor WHERE usuario=@usuario AND senha=@senha";
+                SqlCommand comando = new SqlCommand(sql, conexao);
+
+                comando.Parameters.AddWithValue("@usuario", cbUsuario.Text);
+                comando.Parameters.AddWithValue("@senha", tbSenha.Text);
+
+                conexao.Open();
+                SqlDataReader dados = comando.ExecuteReader();
+                if (dados.Read())
+                {
+                    FormPrincipal Fp = new FormPrincipal();
+                    cbUsuario.Text = "";
+                    tbSenha.Clear();
+                    Fp.ShowDialog();
+                }
+                else
+                    MessageBox.Show("Usuário ou senha incorretos, tente novamente!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message, "Erro na conexão, tente novamente!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {//btLimpar
+            cbUsuario.Text = "";
+            tbSenha.Clear();
+        }
+
+        private void lbReload_Click(object sender, EventArgs e)
+        {//lbReload
+            ProfessorDAO professorDAO = new ProfessorDAO();
+            cbUsuario.DataSource = professorDAO.listarProfs();
+            cbUsuario.DisplayMember = "Usuário";
+            cbUsuario.Text = "";
+            tbSenha.Clear();
+        }
+
+        private void lbVerSenha_Click(object sender, EventArgs e)
+        {//lbVerSenha
+            if (tbSenha.UseSystemPasswordChar.Equals(false))
+                tbSenha.UseSystemPasswordChar = true;
+            else
+                tbSenha.UseSystemPasswordChar = false;
+        }
+
+        #region Sair
+
+        private void FormLogin_KeyDown(object sender, KeyEventArgs e)
+        {//ESC para retornar
+            if (e.KeyValue.Equals(27))
+                Application.Exit();
+        }
+
+        private void lbSair_Click(object sender, EventArgs e)
+        {//lbSair
+            Application.Exit();
+        }
+
+        #endregion
+
+    }
+}
